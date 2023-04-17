a9e0756cb592f8e7749f1950ad42fe8da1175b09
newbee0315 <46306872+newbee0315@users.noreply.github.com>
2023-04-17 15:53:55 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ import time
 latest_iteration = st.empty()
 bar = st.progress(0)
 
-for i in range(10):
+for i in range(100):
   # Update the progress bar with each iteration.
   latest_iteration.text(f'Iteration {i+1}')
   bar.progress(i + 1)
-  time.sleep(0.1)
+  time.sleep(0.03)
 
 '...and now we\'re done!'
 "翁语昕，我爱你哦"
