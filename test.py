ac3f6e4c19d3f291be935918a83e4991ce604b3e
newbee0315 <46306872+newbee0315@users.noreply.github.com>
2023-04-17 15:52:51 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import time
 latest_iteration = st.empty()
 bar = st.progress(0)
 
-for i in range(100):
+for i in range(10):
   # Update the progress bar with each iteration.
   latest_iteration.text(f'Iteration {i+1}')
   bar.progress(i + 1)
