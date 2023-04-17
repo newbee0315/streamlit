bdc355b76dc30378202cc305206a5dd618f8c9f4
wulala <46306872+newbee0315@users.noreply.github.com>
2023-04-17 17:51:47 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -2,35 +2,44 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+import time
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+ 
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+def send_love(name):
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+    print(f"亲爱的{name}，我想告诉你：")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+    time.sleep(2)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    print("我喜欢你！")
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    time.sleep(2)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+    print("喜欢你的笑容、喜欢你的眼神、喜欢你的声音，")
+
+    time.sleep(2)
+
+    print("喜欢你每一个细微的表情和动作。")
+
+    time.sleep(2)
+
+    print("我知道我们之间还有些距离，但我想一步一步走过来，")
+
+    time.sleep(2)
+
+    print("和你一起分享生活的点点滴滴，")
+
+    time.sleep(2)
+
+    print("一起经历生命中的每一个美好瞬间。")
+
+    time.sleep(2)
+
+   
+    
+
+# 在这里输入表白对象的名字
+
+send_love("翁语昕")
+
